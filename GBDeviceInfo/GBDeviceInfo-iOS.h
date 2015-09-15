314dea665237f7d431b4eef3a15ff10982bb45cb
Marc Palmer <marc@anyware.co.uk>
2015-09-15 16:08:27 +0100

    Attempt at dynamic framework for iOS

---
+++ GBDeviceInfo/GBDeviceInfo-iOS.h
@@ -0,0 +1,25 @@
+//
+//  GBDeviceInfo.h
+//  GBDeviceInfo
+//
+//  Created by Marc Palmer on 15/09/2015.
+//  Copyright © 2015 Luka Mirosevic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GBDeviceInfo.
+FOUNDATION_EXPORT double GBDeviceInfoVersionNumber;
+
+//! Project version string for GBDeviceInfo.
+FOUNDATION_EXPORT const unsigned char GBDeviceInfoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GBDeviceInfo/PublicHeader.h>
+
+#import <GBDeviceInfo/GBDeviceInfoInterface.h>
+#import <GBDeviceInfo/GBDeviceInfoTypes_Common.h>
+#import <GBDeviceInfo/GBDeviceInfo_Common.h>
+#import <GBDeviceInfo/GBDeviceInfo_Subclass.h>
+#import <GBDeviceInfo/GBDeviceInfoTypes_iOS.h>
+#import <GBDeviceInfo/GBDeviceInfo_iOS.h>
+
